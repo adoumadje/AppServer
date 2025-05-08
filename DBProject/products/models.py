8bdab14567def4ebcd739a3cf66e184898a8016a
adoumadje <adoumadjecaleb@gmail.com>
2025-05-08 21:16:31 +0200

    DB Project extended to add category

--- DBProject/products/models.py
+++ DBProject/products/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+   name = models.CharField(max_length=100)
+
+   def __str__(self):
+       return self.name
+
 class Product(models.Model):
    name = models.CharField(max_length=255)  # A string field with a max length of 255
    weight = models.FloatField()  # A float field
    production_date = models.DateField()  # A date field
+   category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1);
 
    def __str__(self):
        return f"{self.name} - (weight: {self.weight})"  # This makes the product name appear in Django Admin
+
--- DBProject/products/views.py
+++ DBProject/products/views.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Product
+from .models import Product, Category
 
 
 # Create your views here.
@@ -14,24 +14,31 @@ def product_list(request):
 
 # Create
 def product_create(request):
-   if request.method == 'POST':
-       name = request.POST.get('name')
-       weight = float(request.POST.get('weight'))
-       production_date = request.POST.get('production_date')
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        weight = float(request.POST.get('weight'))
+        production_date = request.POST.get('production_date')
+        category_id = request.POST.get('category')
+        category = get_object_or_404(Category, pk=category_id)
 
-       if weight <= 0 or production_date >= str(date.today()):
-           return render(request, 'products/product_form.html', {'error': 'Invalid data entered.'})
+        if weight <= 0 or production_date >= str(date.today()):
+            return render(request, 'products/product_form.html', {'error': 'Invalid data entered.'})
 
-       Product.objects.create(name=name, weight=weight, production_date=production_date)
-       return redirect('product_list')
-   return render(request, 'products/product_form.html')
+        Product.objects.create(name=name, weight=weight, production_date=production_date, category=category)
+        return redirect('product_list')
+    return render(request, 'products/product_form.html', {'categories': categories})
+
 
 # Update
 def product_update(request, pk):
+   categories = Category.objects.all()
    product = get_object_or_404(Product, pk=pk)
    if request.method == 'POST':
        weight = float(request.POST.get('weight'))
        production_date = request.POST.get('production_date')
+       category_id = request.POST.get('category')
+       category = get_object_or_404(Category, pk=category_id)
 
        if weight <= 0 or production_date >= str(date.today()):
            return render(request, 'products/product_form.html', {'product': product, 'error': 'Invalid data entered.'})
@@ -39,9 +46,10 @@ def product_update(request, pk):
        product.name = request.POST.get('name')
        product.weight = weight
        product.production_date = production_date
+       product.category = category
        product.save()
        return redirect('product_list')
-   return render(request, 'products/product_form.html', {'product': product})
+   return render(request, 'products/product_form.html', {'product': product, 'categories': categories})
 
 # Delete
 def product_delete(request, pk):
