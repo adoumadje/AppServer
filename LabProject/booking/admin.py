e2c6038b21d7be024672c52976997dadd07f7e8c
adoumadje <adoumadjecaleb@gmail.com>
2025-05-18 21:55:24 +0200

    Lab Project: Progressing

--- LabProject/booking/admin.py
+++ LabProject/booking/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from .models import Brand, Features, Color, Car, Booking, SupportTicket
+
+
 # Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Features)
+class FeaturesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SupportTicket)
+class SupportTicketAdmin(admin.ModelAdmin):
+    pass
--- LabProject/booking/models.py
+++ LabProject/booking/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -20,9 +21,44 @@ class Features(models.Model):
     def __str__(self):
         return self.name
 
-class Photo(models.Model):
-    title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='photos/')
-
 class Car(models.Model):
-    pass
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) # Many-To-One
+    brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True) # Many-To-One
+    color = models.ForeignKey(Color, on_delete=models.SET_NULL, null=True) # Many-To-One
+    model = models.CharField(max_length=100)
+    price_per_day=models.DecimalField(max_digits=8, decimal_places=2)
+    seats = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='car_images/') # File support
+    available = models.BooleanField(default=True)
+    features = models.ManyToManyField(Features, help_text='Select one or more features for this car')
+
+    def __str__(self):
+        return f"{self.brand.name} - {self.model}"
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=20, choices=[
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('cancelled', 'Cancelled'),
+    ])
+
+    def __str__(self):
+        return f"Booking by {self.user.username} for {self.car} - {self.status}"
+
+
+class SupportTicket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    status = models.CharField(max_length=20, choices=[
+        ('open', 'Open'),
+        ('closed', 'Closed')
+    ])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Ticket from {self.user.username} : {self.subject}"
