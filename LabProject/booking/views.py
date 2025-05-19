7d54279b0d00478aa9c5ea322362084367e39259
adoumadje <adoumadjecaleb@gmail.com>
2025-05-19 20:37:50 +0200

    Lab Project: auth implemented

--- LabProject/booking/views.py
+++ LabProject/booking/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_user(username=username, password=password)
+            return redirect('accounts/login')
+        else:
+            return render(request, 'registration/register.html',
+                          {'error': 'Username already exists'})
+    return render(request, 'registration/register.html')
