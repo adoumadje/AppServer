5403574b8213f94b80fe045d90e17d2e8591c932
adoumadje <adoumadjecaleb@gmail.com>
2025-05-20 21:35:13 +0200

    Lab Project: finalized auth

--- LabProject/booking/views.py
+++ LabProject/booking/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -13,7 +14,7 @@ def register(request):
         password = request.POST['password']
         if not User.objects.filter(username=username).exists():
             User.objects.create_user(username=username, password=password)
-            return redirect('accounts/login')
+            return redirect(f"{reverse('login')}?next=/booking/")
         else:
             return render(request, 'registration/register.html',
                           {'error': 'Username already exists'})
